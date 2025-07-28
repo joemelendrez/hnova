24f0b3fccaf26d4ad7b89a63a0c649d2aae8c230
joemelendrez <josephmelendrez12@gmail.com>
2025-07-28 16:58:34 -0500

    update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -11,7 +11,7 @@ export default function AboutPage() {
   ]
   
   return (
-    <div className="pt-20 lg:pt-24">
+    <div className="pt-16 lg:pt-20">
       {/* Hero Section */}
       <section className="py-20 bg-white">
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
